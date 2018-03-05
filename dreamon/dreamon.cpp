3fff7cce17c1db8425d562e21d6b9741ad7f1c59
Ran Proshan <ran.proshan@gmail.com>
2018-03-05 22:32:50 +0200

    Data loading dev work

--- dreamon/dreamon.cpp
+++ dreamon/dreamon.cpp
@@ -1,9 +1,14 @@
 
 #include <unistd.h>
+#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 #include <list>
 #include <iostream>
 using namespace std;
+
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include <log4cpp/Category.hh>
 #include <log4cpp/FileAppender.hh>
@@ -16,10 +21,9 @@ using namespace std;
 typedef struct __data_source
 {
 	std::string path;
-	bool directory;
 	
 	__data_source()
-	: path(""), directory(false)
+	: path("")
 	{}
 }data_source_t;
 
@@ -43,15 +47,9 @@ typedef struct __log
 	int level;
 	
 	__log()
-	: file("dreamon.log"), directory("var/log/o4m"), max_files(10), max_size(5*1024*1024), level(3)
+	: file("dreamon.log"), directory("./"), max_files(2), max_size(5*1024*1024), level(3)
 	{}
 }log_t;
-
-void get_options(int argc, char *argv[], data_source_t * dsrc, service_t * svc, log_t * log);
-void show_usage(const char * prog);
-void init_log(const log_t & log);
-void load_data(const data_source_t & dsrc);
-void serve(const service_t & svc);
 
 typedef struct
 {
@@ -72,6 +70,15 @@ typedef struct
 	sample_t samples[SAMPLES_PER_RECORD];
 }record_t;
 
+void get_options(int argc, char *argv[], data_source_t * dsrc, service_t * svc, log_t * log);
+void show_usage(const char * prog);
+void init_log(const log_t & log);
+void load_data(const data_source_t & dsrc);
+void load_data_file(const char * file);
+void load_record(std::list<std::string> & rec_lines);
+int parse_rec_hdr(record_t & rec, std::string & hdr);
+void serve(const service_t & svc);
+
 std::list< record_t > records;
 
 int main(int argc, char *argv[])
@@ -89,7 +96,7 @@ int main(int argc, char *argv[])
 void get_options(int argc, char *argv[], data_source_t * dsrc, service_t * svc, log_t * log)
 {
 	int opt;
-	while ((opt = getopt(argc, argv, "hf:d:a:p:t:l:c:m:s:r:")) != -1)
+	while ((opt = getopt(argc, argv, "hf:a:p:t:l:c:m:s:r:")) != -1)
 	{
 		switch (opt)
 		{
@@ -98,11 +105,6 @@ void get_options(int argc, char *argv[], data_source_t * dsrc, service_t * svc, 
 			exit(0);
 		case 'f':
 			dsrc->path = optarg;
-			dsrc->directory = false;
-			break;
-		case 'd':
-			dsrc->path = optarg;
-			dsrc->directory = true;
 			break;
 		case 'a':
 			svc->address = optarg;
@@ -141,7 +143,6 @@ void show_usage(const char * prog)
 	std::cout << "Usage:" << std::endl;
 	std::cout << prog << "   [ OPTIONS ]" << std::endl;
 	std::cout << "-f   source data file" << std::endl;
-	std::cout << "-d   source data directory" << std::endl;
 	std::cout << "-a   service address" << std::endl;
 	std::cout << "-p   service port" << std::endl;
 	std::cout << "-t   service timeout" << std::endl;
@@ -185,4 +186,89 @@ void init_log(const log_t & log)
     log4cpp::Category::getInstance("drmn").addAppender(appender);
     log4cpp::Category::getInstance("drmn").setPriority((log4cpp::Priority::PriorityLevel)log.level);
     log4cpp::Category::getInstance("drmn").notice("dreamon log start");
+}
+
+void load_data(const data_source_t & dsrc)
+{
+	struct stat dsrc_stat;
+	if(0 == stat(dsrc.path.c_str(), &dsrc_stat))
+	{
+		if(S_ISREG(dsrc_stat.st_mode))
+			load_data_file(dsrc.path.c_str());
+		else
+		{
+			log4cpp::Category::getInstance("drmn.dsrc").fatal("%s: [%s] isn't a file and only source data files are supported.",
+							__FUNCTION__, dsrc.path.c_str());
+			exit(__LINE__);
+		}
+	}
+	else
+	{
+		int errcode = errno;
+		char errmsg[256];
+		log4cpp::Category::getInstance("drmn.dsrc").fatal("%s: stat() of [%s] failed with error %d : [%s]",
+				__FUNCTION__, dsrc.path.c_str(), errcode, strerror_r(errcode, errmsg, 256));
+		exit(__LINE__);
+	}
+}
+
+void load_data_file(const char * file)
+{
+	FILE * pf = fopen(file, "r");
+	if(NULL != pf)
+	{
+		static const char record_token[] = "Time,Price,%,SD";
+		static const size_t record_line_count = 12;
+		char buffer[256];
+		while(NULL != fgets(buffer, 256, pf))
+		{
+			if(NULL != strstr(buffer, record_token))
+			{
+				std::list<std::string> rec_lines;
+				for(size_t i = 0; i < record_line_count && NULL != fgets(buffer, 256, pf); ++i)
+					rec_lines.push_back(buffer);
+				if(rec_lines.size() == record_line_count)
+					load_record(rec_lines);
+			}
+		}
+		fclose(pf);
+	}
+	else
+	{
+		int errcode = errno;
+		char errmsg[256];
+		log4cpp::Category::getInstance("drmn.dsrc").fatal("%s: fopen() of [%s] failed with error %d : [%s]",
+				__FUNCTION__, file, errcode, strerror_r(errcode, errmsg, 256));
+		exit(__LINE__);
+	}
+}
+
+void load_record(std::list<std::string> & rec_lines)
+{
+	static const char samples_token[] = "Call,,Put";
+
+	record_t rec;
+	std::list<std::string>::iterator i = rec_lines.begin();
+
+	if(0 != parse_rec_hdr(rec, *i))
+	{
+		log4cpp::Category::getInstance("drmn.dsrc").error("%s: failed parsing record header [%s]",
+				__FUNCTION__, i->c_str());
+		return;
+	}
+	++i;
+
+	if(NULL == strstr(i->c_str(), samples_token))
+	{
+		log4cpp::Category::getInstance("drmn.dsrc").error("%s: invalid samples token [%s]",
+				__FUNCTION__, i->c_str());
+		return;
+	}
+	++i;
+
+	while(rec_lines.end() != i)
+	{
+#error 'continue here'
+	}
+
 }
